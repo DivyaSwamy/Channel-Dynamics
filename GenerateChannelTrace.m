7c467173ce238c3a854be2ce14978a2619bc0a34
Divya Swaminathan <sdivya.swaminathan@gmail.com>
2018-03-20 14:34:45 -0700

    Add files via upload

---
+++ GenerateChannelTrace.m
@@ -0,0 +1,31 @@
+function [Channel_Trace,OpenTimes,CloseTimes] = GenerateChannelTrace(tauop,tauclose,sample_time,total_time)
+% March 20th 2018@ DS
+% To generate a binary open close trace given that the channel
+% has a mean open time of  tauop ms and a mean close time
+% of taucl. 
+% Sample_time = frame rate of experiment
+% Total_time = total time you want to simulate the trace for
+
+Nsteps = ceil(total_time./sample_time);
+
+% Generate Open Times
+OpenTimes = -log(rand(1,Nsteps))*tauop; 
+OpenTimes = ceil(OpenTimes./sample_time);
+
+% Generate Close Times
+CloseTimes = -log(rand(1,Nsteps))*tauclose;
+CloseTimes = ceil(CloseTimes./sample_time);
+
+% Put it together as sample trace.
+Channel_Trace = zeros(1,Nsteps);
+count = 1; tab = CloseTimes(count)+1;
+while  tab <= Nsteps
+       index_open = tab:tab+OpenTimes(count)-1;
+       Channel_Trace(1,index_open)=1.0;
+       tab = tab+OpenTimes(count)+CloseTimes(count+1);
+       count=count+1;
+end
+
+return
+end
+
